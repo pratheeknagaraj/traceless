6b63c534f0f4fe4a936c921e7569fc8242f6da60
Nikhil Buduma <nkbuduma@gmail.com>
2015-05-08 18:36:49 -0400

    pushes and pulls now updated

--- slave/app/pulls/routes.py
+++ slave/app/pulls/routes.py
@@ -6,21 +6,45 @@ from .. import traceless_crypto
 
 @pulls.route('/pull', methods=['POST'])
 def pull():
-    server_seen_nonces = app.jinja_env.globals['server_seen_nonces']
-    server_seen_nonces_lock = app.jinja_env.globals['server_seen_nonces_lock']
-    with server_seen_nonces_lock:
+    with app.jinja_env.globals['server_view_manager_lock']:
+        server_seen_nonces = app.jinja_env.globals['server_seen_nonces']
+        
         if not request.json or not traceless_crypto.verify(request.json['nonce'], request.json['signature']):
             abort(400)
     
         if request.json['nonce'] in server_seen_nonces:
             return server_seen_nonces[request.json['nonce']]
-            
-        server_messages_table = app.jinja_env.globals['server_messages_table']
-        server_messages_table_lock = app.jinja_env.globals['server_messages_table_lock']
-        with server_messages_table_lock:
-            server_seen_nonces[request.json['nonce']] = jsonify({'messages' : server_messages_table[request.json['slot_id']],
+
+        shard = literal_eval(app.jinja_env.globals['shard'])
+        if request.json['slot_id'] < shard[0] or  request.json['slot_id'] >= shard[1] \
+                or app.jinja_env.globals['server_views'][shard]['P'] != app.jinja_env.globals['server_me_url']
+            server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
                                                                 'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
             return server_seen_nonces[request.json['nonce']]
+
+        try:
+            args = {
+                'messages_table' :  {}
+            }
+            response = requests.post(app.jinja_env.globals['server_master_url'] + "/process_forward", headers=headers, data=json.dumps(args))
+            r = json.loads(response.text)
+            if r['success'] == False:
+                server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                    'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+                return server_seen_nonces[request.json['nonce']]
+        except requests.exceptions.RequestException as e:
+            server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+            return server_seen_nonces[request.json['nonce']]
+            
+        server_messages_table = app.jinja_env.globals['server_messages_table']
+        if request.json['slot_id'] in server_messages_table:             
+            server_seen_nonces[request.json['nonce']] = jsonify({'messages' : server_messages_table[request.json['slot_id']],
+                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+        else:
+            server_seen_nonces[request.json['nonce']] = jsonify({'messages' : [],
+                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+        return server_seen_nonces[request.json['nonce']]
 
 # @pulls.route('/delete', methods=['POST'])
 # def delete():
--- slave/app/pushes/routes.py
+++ slave/app/pushes/routes.py
@@ -34,48 +34,54 @@ from ast import literal_eval
 
 @pushes.route('/push', methods=['POST'])
 def push():
-    server_seen_nonces = app.jinja_env.globals['server_seen_nonces']
-    if not request.json or not traceless_crypto.verify(request.json['nonce'], request.json['signature']):
-        abort(400)
-    
-    if request.json['nonce'] in server_seen_nonces:
-        return server_seen_nonces[request.json['nonce']]
-    
-    shard = literal_eval(app.jinja_env.globals['shard'])
-    if request.json['slot_id'] < shard[0] or  request.json['slot_id'] >= shard[1]:
-        server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+    with app.jinja_env.globals['server_view_manager_lock']:
+        server_seen_nonces = app.jinja_env.globals['server_seen_nonces']
+        if not request.json or not traceless_crypto.verify(request.json['nonce'], request.json['signature']):
+            abort(400)
+        
+        if request.json['nonce'] in server_seen_nonces:
+            return server_seen_nonces[request.json['nonce']]
+        
+        shard = literal_eval(app.jinja_env.globals['shard'])
+        if request.json['slot_id'] < shard[0] or  request.json['slot_id'] >= shard[1] \
+                or app.jinja_env.globals['server_views'][shard]['P'] != app.jinja_env.globals['server_me_url']
+            server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+            return server_seen_nonces[request.json['nonce']]
+
+        server_messages_table = app.jinja_env.globals['server_messages_table']
+        if request.json['slot_id'] in server_messages_table:
+            try:
+                args = {
+                    'messages_table' :  {request.json['slot_id'] : server_messages_table[request.json['slot_id']] + [request.json['message']]}
+                }
+                response = requests.post(app.jinja_env.globals['server_master_url'] + "/process_forward", headers=headers, data=json.dumps(args))
+                r = json.loads(response.text)
+                if r['success'] == False:
+                    server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                        'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+                    return server_seen_nonces[request.json['nonce']]
+            except requests.exceptions.RequestException as e:
+                server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                    'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+                return server_seen_nonces[request.json['nonce']]
+            server_messages_table[request.json['slot_id']].append(request.json['message'])
+        else:
+            try:
+                args = {
+                    'messages_table' : {request.json['slot_id'] : [request.json['message']]}
+                }
+                response = requests.post(app.jinja_env.globals['server_master_url'] + "/process_forward", headers=headers, data=json.dumps(args))
+                r = json.loads(response.text)
+                if r['success'] == False:
+                    server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                        'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+                    return server_seen_nonces[request.json['nonce']]
+            except requests.exceptions.RequestException as e:
+                server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
+                                                                    'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
+                return server_seen_nonces[request.json['nonce']]
+            server_messages_table[request.json['slot_id']] = [request.json['message']]
+        server_seen_nonces[request.json['nonce']] = jsonify({'success' : True,
                                                             'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
         return server_seen_nonces[request.json['nonce']]
-
-    server_messages_table = app.jinja_env.globals['server_messages_table']
-    if request.json['slot_id'] in server_messages_table:
-        try:
-            args = {
-                'messages_table' :  server_messages_table[request.json['slot_id']] + [request.json['message']]
-            }
-            response = requests.post(app.jinja_env.globals['server_master_url'] + "/process_forward", headers=headers, data=json.dumps(args))
-            r = json.loads(response.text)
-            if r['success'] == False:
-                return jsonify({'success' : False}), 200
-        except requests.exceptions.RequestException as e:
-            server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
-                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
-            return server_seen_nonces[request.json['nonce']]
-        server_messages_table[request.json['slot_id']].append(request.json['message'])
-    else:
-        try:
-            args = {
-                'messages_table' : [request.json['message']]
-            }
-            response = requests.post(app.jinja_env.globals['server_master_url'] + "/process_forward", headers=headers, data=json.dumps(args))
-            r = json.loads(response.text)
-            if r['success'] == False:
-                return jsonify({'success' : False}), 200
-        except requests.exceptions.RequestException as e:
-            server_seen_nonces[request.json['nonce']] = jsonify({'success' : False,
-                                                                'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
-            return server_seen_nonces[request.json['nonce']]
-        server_messages_table[request.json['slot_id']] = [request.json['message']]
-    server_seen_nonces[request.json['nonce']] = jsonify({'success' : True,
-                                                        'blinded_sign' : traceless_crypto.ust_sign(request.json['blinded_nonce'])}), 200
-    return server_seen_nonces[request.json['nonce']]
